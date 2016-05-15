b3da86d7f6eb2efa5f5cc1cb41e05f6d82e8f729
Bartosz Łęcki <bartosz.lecki@netguru.pl>
2016-05-15 01:31:42 +0100

    Add new and create action for props

--- web/controllers/props_controller.ex
+++ web/controllers/props_controller.ex
@@ -1,3 +1,4 @@
+
 defmodule Props.PropsController do
   use Props.Web, :controller
 
@@ -8,5 +9,27 @@ defmodule Props.PropsController do
   def index(conn, _params) do
     props = Prop |> Repo.all |> Repo.preload([:user, :propser])
     render conn, "index.html", props: props
+  end
+
+  def new(conn, _params) do
+    changeset = Prop.changeset(%Prop{}, %{})
+    render conn, "new.html", changeset: changeset, users_list: users_list
+  end
+
+  def create(conn, %{"prop" => prop_params}) do
+    propser = get_session(conn, :current_user)
+    changeset = Prop.changeset(%Prop{propser_id: propser.id, user_id: prop_params["user_id"]}, prop_params)
+    case changeset.valid? do
+      true ->
+        Repo.insert(changeset)
+        redirect conn, to: props_path(conn, :index)
+      false ->
+        render conn, "edit.html", changeset: changeset, errors: changeset.errors
+    end
+  end
+
+  defp users_list do
+    Repo.all(User) |>
+      Enum.map(&{User.name(&1), &1.id})
   end
 end
